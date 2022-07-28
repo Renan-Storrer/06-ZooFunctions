44001291a04931aacd9a39c07ef0fd98a862d8d3
Renan Storrer <storrerrenan@gmail.com>
2022-07-28 18:53:26 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const todosAnimais = data.species;
+  if (animal === undefined) {
+    const result = {};
+    todosAnimais.forEach((elemento) => {
+      result[[elemento.name]] = elemento.residents.length;
+    });
+    return result;
+  }
+  const { specie, sex = 'sexoDoAnimal' } = animal;
+  const animalEscolhido = todosAnimais.find((elemento) => elemento.name === specie);
+  if (sex === 'sexoDoAnimal') {
+    return animalEscolhido.residents.length;
+  }
+  return animalEscolhido.residents.filter(({ sex: sexo }) => sexo === sex).length;
 }
 
 module.exports = countAnimals;
