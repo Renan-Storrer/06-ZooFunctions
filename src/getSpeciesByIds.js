4dafd9830b6b64c43f01cb98297ee480555ed4ef
Renan Storrer <storrerrenan@gmail.com>
2022-07-28 15:34:49 -0300

    requisito 1***

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return data.species.filter((animal) => ids.includes(animal.id));
+  return data.species.filter((animalAtual) => ids.includes(animalAtual.id));
 }
 
 module.exports = getSpeciesByIds;
