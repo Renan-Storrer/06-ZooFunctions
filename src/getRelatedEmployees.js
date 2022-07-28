0bdd570e49874514f6b91519cb805a2272c6cf02
Renan Storrer <storrerrenan@gmail.com>
2022-07-28 18:24:07 -0300

    erros lint req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -9,11 +9,11 @@ function getRelatedEmployees(managerId) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   return data.employees.reduce((acc, val) => {
-    const { managers, firstName, lastName} = val;
+    const { managers, firstName, lastName } = val;
     if (managers.includes(managerId)) {
-      acc.push(`${firstName} ${lastName}`)
-    } return acc
-  }, [])
+      acc.push(`${firstName} ${lastName}`);
+    } return acc;
+  }, []);
 }
 
 module.exports = { isManager, getRelatedEmployees };
