1af079d7fa9399fc783e17d93c49d2989c54822e
Renan Storrer <storrerrenan@gmail.com>
2022-08-01 02:41:11 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
+function idade(entrants) {
+  let seniorC = 0;
+  let AdultC = 0;
+  let childC = 0;
+  entrants.forEach((visitant) => {
+    if (visitant.age < 18) {
+      childC = childC + 1;
+    }
+    if (visitant.age >= 18 && visitant.age < 50) {
+      AdultC = AdultC + 1;
+    };
+    if (visitant.age >= 50) {
+      seniorC = seniorC + 1;
+    }
+  });
+  return [childC, AdultC, seniorC];
+};
+
 function countEntrants(entrants) {
-  // seu código aqui
-}
+  const [ childC, AdultC, seniorC ] = idade(entrants);
+  return { child: childC, adult: AdultC, senior: seniorC };
+};
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const [childC, AdultC, seniorC] = idade(entrants);
+  return (childC * 20.99) + (AdultC * 49.99) + (seniorC * 24.99)
 }
 
 module.exports = { calculateEntry, countEntrants };
