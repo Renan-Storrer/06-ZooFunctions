0e14a44a2e43c1f20bdccf65047fb16ac1f5a695
Renan Storrer <storrerrenan@gmail.com>
2022-08-01 02:42:52 -0300

    erros lint requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -6,27 +6,27 @@ function idade(entrants) {
   let childC = 0;
   entrants.forEach((visitant) => {
     if (visitant.age < 18) {
-      childC = childC + 1;
+      childC += 1;
     }
     if (visitant.age >= 18 && visitant.age < 50) {
-      AdultC = AdultC + 1;
-    };
+      AdultC += 1;
+    }
     if (visitant.age >= 50) {
-      seniorC = seniorC + 1;
+      seniorC += 1;
     }
   });
   return [childC, AdultC, seniorC];
-};
+}
 
 function countEntrants(entrants) {
-  const [ childC, AdultC, seniorC ] = idade(entrants);
+  const [childC, AdultC, seniorC] = idade(entrants);
   return { child: childC, adult: AdultC, senior: seniorC };
-};
+}
 
 function calculateEntry(entrants) {
   if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
   const [childC, AdultC, seniorC] = idade(entrants);
-  return (childC * 20.99) + (AdultC * 49.99) + (seniorC * 24.99)
+  return (childC * 20.99) + (AdultC * 49.99) + (seniorC * 24.99);
 }
 
 module.exports = { calculateEntry, countEntrants };
