41e23e093d8e0832e57252a36a83971180cc3604
Renan Storrer <storrerrenan@gmail.com>
2022-08-01 02:28:46 -0300

    requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,18 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('O parametro não é uma string?', () => {
+    expect(handlerElephants(459)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Parametro é uma string?', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toEqual(['Bea', 'Orval', 'Jefferson', 'Ilana']);
+    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Retorna "UNDEFINED" se a função não tem parametro?', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+});
