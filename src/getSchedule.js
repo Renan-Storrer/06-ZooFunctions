5370b4acd69b8b4645ae1ba3b6d11e15060244c4
Renan Storrer <storrerrenan@gmail.com>
2022-08-01 15:17:53 -0300

    Requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
+const diaSemana = (dia) => {
+  if (dia === 'Monday') {
+    return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  }
+  const animals = species.filter((element) => element.availability.includes(dia));
+  const exhibition = animals.map((element) => element.name);
+  const officeHour = `Open from ${hours[dia].open}am until ${hours[dia].close}pm`;
+  const result = {
+    officeHour,
+    exhibition,
+  };
+  return result;
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const dias = Object.keys(hours);
+  if (dias.includes(scheduleTarget)) {
+    const scheduleOfTheDay = {};
+    scheduleOfTheDay[scheduleTarget] = diaSemana(scheduleTarget);
+    return scheduleOfTheDay;
+  }
+  if (!scheduleTarget || !species.some((element) => element.name === scheduleTarget)) {
+    const availability = {};
+    for (let i = 0; i < dias.length; i += 1) {
+      availability[dias[i]] = (diaSemana(dias[i]));
+    }
+    return availability;
+  }
+  return species.find((specie) => specie.name === scheduleTarget).availability;
 }
 
 module.exports = getSchedule;
